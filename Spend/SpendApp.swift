529bcfd6a3690f5e929b345d04ef772bcc3abdf5
Dmitry Schelochkov <ddimpalass@gmail.com>
2021-05-08 12:08:49 +0300

    Initial Commit

---
+++ Spend/SpendApp.swift
@@ -0,0 +1,20 @@
+//
+//  SpendApp.swift
+//  Spend
+//
+//  Created by Дмитрий on 08.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct SpendApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
